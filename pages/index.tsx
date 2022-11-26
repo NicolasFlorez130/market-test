c53103d7a0b3ae4e4d0a838aaf4ea6f84c11eec6
nmflorezr <nmflorezr@gmail.com>
2022-11-26 16:13:05 -0500

    Performance improved by changing awaits for promises wrapped by a Promise.all in the getSSP in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,8 @@ export interface Filters {
 }
 
 const Home = ({
-      filters,
-    }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  filters,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const [user, setUser] = useState();
 
   return (
@@ -53,27 +53,38 @@ export default Home;
 export const getServerSideProps: GetServerSideProps = async () => {
   const prisma = new PrismaClient();
 
-  const brand = await prisma.brand.findMany();
-  const vehicleType = await prisma.vehicleType.findMany();
-  const body = await prisma.body.findMany();
-  const cilinderCapacity = await prisma.cilinderCapacity.findMany();
-  const transmision = await prisma.transmision.findMany();
-  const tractionType = await prisma.tractionType.findMany();
-  const fuelType = await prisma.fuelType.findMany();
-  const vehicleModel = await prisma.vehicleModel.findMany();
+  const brand = prisma.brand.findMany().then();
+  const vehicleType = prisma.vehicleType.findMany().then();
+  const body = prisma.body.findMany().then();
+  const cilinderCapacity = prisma.cilinderCapacity.findMany().then();
+  const transmision = prisma.transmision.findMany().then();
+  const tractionType = prisma.tractionType.findMany().then();
+  const fuelType = prisma.fuelType.findMany().then();
+  const vehicleModel = prisma.vehicleModel.findMany().then();
+
+  const filters = await Promise.all([
+    brand,
+    vehicleType,
+    body,
+    cilinderCapacity,
+    transmision,
+    tractionType,
+    fuelType,
+    vehicleModel,
+  ]).then((res) => ({
+    brand: res[0],
+    vehicleType: res[1],
+    body: res[2],
+    cilinderCapacity: res[3],
+    transmision: res[4],
+    tractionType: res[5],
+    fuelType: res[6],
+    vehicleModel: res[7],
+  }));
 
   return {
     props: {
-      filters: {
-        brand,
-        vehicleType,
-        body,
-        cilinderCapacity,
-        transmision,
-        tractionType,
-        fuelType,
-        vehicleModel,
-      },
+      filters,
     },
   };
 };
